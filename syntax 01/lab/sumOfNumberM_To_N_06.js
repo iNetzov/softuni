89121a899422234b25040917dda52964ff5938ea
iNetzov <ivn9812@gmail.com>
2021-06-03 17:41:55 +0300

    Js Advanced Homeworks from syntax Lab 01

---
+++ syntax 01/lab/sumOfNumberM_To_N_06.js
@@ -0,0 +1,12 @@
+function solve(num1, num2){
+    num1 = Number(num1);
+    num2 = Number(num2);
+    let result= 0;
+    for(let i = num1; i<=num2; i++){
+        result = result + i;
+        }
+
+console.log(result);
+}
+
+solve('1','5');
