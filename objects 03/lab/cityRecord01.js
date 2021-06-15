9fbc27fcf80075d58768c166ac5ff8001013c62a
iNetzov <ivn9812@gmail.com>
2021-06-15 20:53:56 +0300

    Js advanced object lab

---
+++ objects 03/lab/cityRecord01.js
@@ -0,0 +1,10 @@
+function solve (name,population ,treasury){
+    const city = {
+        name:name,
+        population:population,
+        treasury:treasury,
+    }
+    return city;
+}
+
+console.log(solve('sofia',4200,'bulgarian '))
