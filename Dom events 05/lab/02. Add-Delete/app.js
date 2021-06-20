32995596dad67c398b869ca2b107b27fdb225c08
iNetzov <ivn9812@gmail.com>
2021-06-20 14:09:39 +0300

    taks 02 from Dom events

--- Dom events 05/lab/02. Add-Delete/app.js
+++ Dom events 05/lab/02. Add-Delete/app.js
@@ -3,6 +3,13 @@ function addItem() {
     let textBox = document.querySelector('#newItemText');
     let newElementAdd = document.createElement('li');
     newElementAdd.textContent = textBox.value;
+    let deleteButton = document.createElement('a');
+    deleteButton.setAttribute('href','#');
+    deleteButton.textContent ='[Delete]'
+    deleteButton.addEventListener('click',(e)=>{e.currentTarget.parentNode.remove()});
+    newElementAdd.appendChild(deleteButton);
+
     listArray.appendChild(newElementAdd);
-    textBox.value = ''; 
+   
+    
 }
