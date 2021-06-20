1de2a3fbf3ba78952481b5217150af9a96f288fb
iNetzov <ivn9812@gmail.com>
2021-06-20 15:37:25 +0300

    tasks from 3-6 Dom Events Js Advanced

--- Dom events 05/lab/03. Delete-From-Table/app.js
+++ Dom events 05/lab/03. Delete-From-Table/app.js
@@ -1,3 +1,20 @@
+
 function deleteByEmail() {
-    console.log('TODO:...');
-}
+    const getTextField = document.querySelector('input').value;
+    const getElements = document.getElementsByTagName('tr');
+    const getMessage = document.getElementById('result');
+    for (const getE of getElements){
+        let elValue  = getE.textContent;
+    
+    if(elValue.includes(getTextField)){
+        getE.remove();
+        getMessage.textContent = 'Deleted.';
+    }
+    }
+    if(getMessage.textContent === 'Deleted.'){
+    
+    }else {
+        getMessage.textContent = `${getTextField} Not found.`;
+    }
+    }
+
--- Dom events 05/lab/06. Dynamic-Validation/app.js
+++ Dom events 05/lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,12 @@
 function validate() {
-    console.log('TODO:...');
+    let emailBox = document.querySelector('#email');
+    //onsole.log(emailBox);
+    emailBox.addEventListener('change',(ev)=>{const email =  ev.target.value;
+        const isitTrue = /^[a-z]+@[a-z]+\.[a-z]+$/.test(email);
+        if( /^[a-z]+@[a-z]+\.[a-z]+$/.test(email)){
+        ev.target.className = '';
+        }else {
+        ev.target.className = 'error';
+        }
+    })
 }
