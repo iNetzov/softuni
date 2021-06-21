e66b6a9cbb56be11999c55736e4e8599d2e1f883
iNetzov <ivn9812@gmail.com>
2021-06-21 14:11:21 +0300

    task 04 dom events exersise

--- Dom events 05/exersise/04. Fill-Dropdown/dropdown.js
+++ Dom events 05/exersise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,11 @@
 function addItem() {
-    console.log('TODO:...');
+    let textBox = document.querySelector('#newItemText');
+    let valueBox = document.querySelector('#newItemValue');
+    let menu = document.querySelector('#menu')
+    let option = document.createElement('option');
+    option.textContent = textBox.value;
+    option.value = valueBox.value;
+    menu.appendChild(option);
+    textBox.value = '';
+    valueBox.value = '';
 }
