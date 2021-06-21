98a9c201a8d361b79c36b858ea599bd6bb3f1f61
iNetzov <ivn9812@gmail.com>
2021-06-21 10:13:21 +0300

    task 02 Dom Events exersise

--- Dom events 05/exersise/02. Time-Converter/solution.js
+++ Dom events 05/exersise/02. Time-Converter/solution.js
@@ -1,4 +1,52 @@
 function attachEventsListeners() {
+    let buttonList = document.querySelectorAll('input[type = button]');
+    //console.log(buttonList);
+    for (const button of buttonList) {
+        button.addEventListener('click', onClick);
+    }
 
-    console.log('TODO:...');
+
+    function onClick(ev) {
+        let currentButton = ev.target.parentNode
+        let check = currentButton.children[0].textContent;
+        let dTextBoxValue = document.querySelector('#days');
+        let hTextBoxValue = document.querySelector('#hours');
+        let mTextBoxValue = document.querySelector('#minutes');
+        let sTextBoxValue = document.querySelector('#seconds');
+
+        if (check === 'Days: ') {
+            let days = Number(dTextBoxValue.value);
+            let hours = days * 24;
+            let minutes = hours * 60;
+            let seconds = minutes * 60;
+            hTextBoxValue.value = hours;
+            mTextBoxValue.value = minutes;
+            sTextBoxValue.value = seconds;
+
+        } else if (check === 'Hours: ') {
+            let hours = Number(hTextBoxValue.value);
+            let days = hours / 24;
+            let minutes = hours * 60;
+            let seconds = minutes * 60;
+            dTextBoxValue.value = days;
+            mTextBoxValue.value = minutes;
+            sTextBoxValue.value = seconds;
+        } else if (check === 'Minutes: ') {
+            let minutes = Number(mTextBoxValue.value);
+            let hours = minutes / 60;
+            let days = hours / 24;
+            let seconds = minutes * 60
+            hTextBoxValue.value = hours;
+            dTextBoxValue.value = days;
+            sTextBoxValue.value = seconds;
+        } else if (check === 'Seconds: ') {
+            let seconds = Number(sTextBoxValue.value);
+            let minutes = seconds / 60;
+            let hours = minutes / 60;
+            let days = hours / 24;
+            mTextBoxValue.value = minutes;
+            hTextBoxValue.value = hours;
+            dTextBoxValue.value = days;
+        }
+    }
 }
