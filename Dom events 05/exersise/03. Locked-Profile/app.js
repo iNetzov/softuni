6db7d8f58887acaa7a5f8c7bf48efed33e99e9bb
iNetzov <ivn9812@gmail.com>
2021-06-21 12:26:14 +0300

    03 task dom events

--- Dom events 05/exersise/03. Locked-Profile/app.js
+++ Dom events 05/exersise/03. Locked-Profile/app.js
@@ -1,3 +1,20 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons = document.querySelectorAll('button');
+    for (const btn of buttons) {
+        btn.addEventListener('click',(e)=>{
+            let div = e.target.parentNode;
+            //console.log(div);
+            let isItLocked = div.children[2].checked;
+            if(isItLocked !== true){
+            let innerDiv = div.querySelector('div');
+            innerDiv.style.display = 'inline';
+            if(e.target.textContent.includes('Show more')){
+            e.target.textContent = 'Hide it';
+            }else{
+                e.target.textContent = 'Show more';
+                innerDiv.style.display = 'none';
+            }
+            }
+        })
+    }
 }
