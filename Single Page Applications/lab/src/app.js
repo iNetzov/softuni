6552b151e1c792eb5ec7a0f281eb048d9d1a17ab
iNetzov <ivn9812@gmail.com>
2021-07-15 23:27:35 +0300

    Demo Single Page Applications JS Applications

---
+++ Single Page Applications/lab/src/app.js
@@ -0,0 +1,7 @@
+
+import printName from './utils.js';
+
+
+let rootElement = document.querySelector('.root')
+        console.log(rootElement)
+        printName();
---
+++ Single Page Applications/lab/src/utils.js
@@ -0,0 +1,11 @@
+function printName(){
+    console.log('print from Utils');
+}
+export default printName;
+ 
+
+
+
+
+//module.exports = printName; 
+ //the 2 things are equal
