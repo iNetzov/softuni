df998e657d78f7e3853d13cf11ed4f427caebfd4
iNetzov <ivn9812@gmail.com>
2021-09-30 18:46:07 +0300

    some changes

--- lecture 4 MVC/mobillelele/mobilelele/src/main/java/bg/softuni/mobilelele/web/TestController.java
+++ lecture 4 MVC/mobillelele/mobilelele/src/main/java/bg/softuni/mobilelele/web/TestController.java
@@ -1,16 +1,18 @@
 package bg.softuni.mobilelele.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController {
 
     @GetMapping("/test")
-    public ModelAndView test(ModelAndView modelAndView){
-        modelAndView.addObject("line","<h2>line 2</h2>");
-        modelAndView.addObject("personToGreed","Banda");
+    public ModelAndView test(ModelAndView modelAndView,@RequestParam(value = "name",defaultValue = "misho")String name){
+
+        modelAndView.addObject("personToGreed",name);
         modelAndView.setViewName("test");
         return modelAndView;
     }
