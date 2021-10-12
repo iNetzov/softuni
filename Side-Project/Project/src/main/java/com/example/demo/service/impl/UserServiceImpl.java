b81b6319aeb8fccb4fe324de64416bc83f1fef94
iNetzov <ivn9812@gmail.com>
2021-10-12 22:07:45 +0300

    Working login form logout+profile

--- Side-Project/Project/src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ Side-Project/Project/src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -5,6 +5,8 @@ import com.example.demo.models.entity.enums.LevelEnum;
 import com.example.demo.models.service.UserServiceModel;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
+import com.example.demo.util.CurrentUser;
+import org.apache.catalina.User;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
@@ -13,10 +15,12 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
+    private final CurrentUser currentUser;
 
-    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper) {
+    public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper, CurrentUser currentUser) {
         this.userRepository = userRepository;
         this.modelMapper = modelMapper;
+        this.currentUser = currentUser;
     }
 
     @Override
@@ -24,5 +28,36 @@ public class UserServiceImpl implements UserService {
         UserEntity user = modelMapper.map(userServiceModel,UserEntity.class);
         user.setLevel(LevelEnum.BEGINNER);
         userRepository.save(user);
+    }
+
+    @Override
+    public UserServiceModel findByUsernameAndPassword(String username, String password) {
+        return userRepository
+                .findByUsernameAndPassword(username,password)
+                .map(UserEntity->modelMapper.map(UserEntity,UserServiceModel.class))
+                .orElse(null);
+    }
+
+    @Override
+    public void loginUser(Long id, String username) {
+        currentUser.setUsername(username);
+        currentUser.setId(id);
+
+
+    }
+
+    @Override
+    public void logout() {
+        currentUser.setUsername(null);
+        currentUser.setId(null);
+    }
+
+    @Override
+    public UserServiceModel findbyId(Long id) {
+       return userRepository
+               .findById(id)
+               .map(UserEntity -> modelMapper
+                       .map(UserEntity,UserServiceModel.class))
+               .orElse(null);
     }
 }
--- Side-Project/Project/src/main/java/com/example/demo/web/UserController.java
+++ Side-Project/Project/src/main/java/com/example/demo/web/UserController.java
@@ -1,17 +1,19 @@
 package com.example.demo.web;
 
+import com.example.demo.models.binding.UserLoginBindingModel;
 import com.example.demo.models.binding.UserRegisterBindingModel;
 import com.example.demo.models.service.UserServiceModel;
+import com.example.demo.models.view.UserViewModel;
 import com.example.demo.service.UserService;
+import org.dom4j.rule.Mode;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/users")
@@ -25,16 +27,20 @@ public class UserController {
     }
 
     @ModelAttribute
+    public UserLoginBindingModel userLoginBindingModel() {return new UserLoginBindingModel();}
+
+    @ModelAttribute
     public UserRegisterBindingModel userRegisterBindingModel(){
         return new UserRegisterBindingModel();
     }
 
+//register requests
     @GetMapping("/register")
     public String register(Model model){
         return "register";
     }
     @PostMapping("/register")
-    public String confirmRegister(UserRegisterBindingModel userRegisterBindingModel,
+    public String registerConfirm(@Valid UserRegisterBindingModel userRegisterBindingModel,
                                   BindingResult bindingResult, RedirectAttributes redirectAttributes){
 
         if (bindingResult.hasErrors()||
@@ -52,8 +58,45 @@ public class UserController {
        return "redirect:login";
     }
 
+   //login requests
+
+
     @GetMapping("/login")
-    public String login(){
+    public String login(Model model){
+        model.addAttribute("isExist",true);
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginConfirm(@Valid UserLoginBindingModel userLoginBindingModel,
+                               BindingResult bindingResult,RedirectAttributes redirectAttributes ){
+        if (bindingResult.hasErrors()){
+            redirectAttributes
+                    .addFlashAttribute("userLoginBindingModel",userLoginBindingModel)
+                    .addFlashAttribute("org.springframework.validation.BindingResult.userLoginBindingModel",bindingResult);
+            return "redirect:login";
+        }
+        UserServiceModel user = userService.findByUsernameAndPassword(
+                userLoginBindingModel.getUsername()
+                ,userLoginBindingModel.getPassword());
+        if (user == null){
+            redirectAttributes
+                    .addFlashAttribute("isExist",false)
+                    .addFlashAttribute("userLoginBindingModel",userLoginBindingModel)
+                    .addFlashAttribute("org.springframework.validation.BindingResult.userLoginBindingModel",bindingResult);
+        return "redirect:login";
+        }
+        userService.loginUser(user.getId(),user.getUsername());
+        return "redirect:/";
+    }
+    @GetMapping("/logout")
+    public String logout(){
+        userService.logout();
+        return "redirect:/";
+    }
+    @GetMapping("/profile/{id}")
+    public String profile(@PathVariable Long id, Model model){
+        model.addAttribute("user",modelMapper.map(userService.findbyId(id), UserViewModel.class));
+        return "profile";
     }
 }
