d99dda06bab734b9fda5863c81d73ec889993167
iNetzov <ivn9812@gmail.com>
2021-11-04 18:57:32 +0200

    This is Rest Api With rest Controllers

--- Rest api/Project/src/main/java/com/example/demo/service/impl/AuthorServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service.impl;
-
-import com.example.demo.service.AuthorService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class AuthorServiceImpl implements AuthorService {
-}
