29b809f15703695c0778282f20da404483e3afaf
iNetzov <ivn9812@gmail.com>
2021-12-08 01:12:53 +0200

    Working on the Js Part of Profile now working

---
+++ ProjectCoffeeShop/src/main/java/com/example/demo/web/ProfileController.java
@@ -0,0 +1,18 @@
+package com.example.demo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class ProfileController {
+
+
+
+    @GetMapping("/profile")
+    public String profile(){
+
+        return "profile";
+    }
+}
