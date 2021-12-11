2265f400f8c2c2fe004f05b030c7275ba9a257cf
iNetzov <ivn9812@gmail.com>
2021-12-11 16:21:21 +0200

    Adding / to controller

--- ProjectCoffeeShop/src/main/java/com/example/demo/web/LogController.java
+++ ProjectCoffeeShop/src/main/java/com/example/demo/web/LogController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class LogController {
 
 
-    @GetMapping("logs")
+    @GetMapping("/logs")
     public String logs(){
         return "log-all";
     }
