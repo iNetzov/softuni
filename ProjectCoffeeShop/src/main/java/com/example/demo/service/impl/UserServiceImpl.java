6e0295e1b93db2ac7a21520760c9e77b4ac6bf73
iNetzov <ivn9812@gmail.com>
2022-01-31 22:31:04 +0200

    refactor on the init Mehtod that adds users to empty database

--- ProjectCoffeeShop/src/main/java/com/example/demo/service/impl/UserServiceImpl.java
+++ ProjectCoffeeShop/src/main/java/com/example/demo/service/impl/UserServiceImpl.java
@@ -1,26 +1,19 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.models.entity.ProductEntity;
 import com.example.demo.models.entity.RoleEntity;
 import com.example.demo.models.entity.UserEntity;
 import com.example.demo.models.entity.enums.RoleEntityNameEnum;
 import com.example.demo.models.service.UserServiceModel;
-import com.example.demo.models.view.ProductsAllViewModel;
 import com.example.demo.models.view.UserManageViewModel;
-import com.example.demo.repository.RoleRepository;
 import com.example.demo.repository.UserRepository;
 import com.example.demo.service.RoleService;
 import com.example.demo.service.UserService;
-import com.example.demo.web.exeptions.ObjectNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
@@ -29,6 +22,8 @@ public class UserServiceImpl implements UserService {
     private final ModelMapper modelMapper;
     private final RoleService roleService;
     private final PasswordEncoder passwordEncoder;
+    private final String PASSWORD ="12345";
+    private final String USER_PIC_URL ="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.subpng.com%2Fpng-9lw7jx%2F&psig=AOvVaw0c6sFiecNtCHsdCz5bBcVU&ust=1636762391519000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKCY_pvFkfQCFQAAAAAdAAAAABAD";
 
     public UserServiceImpl(UserRepository userRepository, ModelMapper modelMapper, RoleService roleService, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
@@ -40,13 +35,11 @@ public class UserServiceImpl implements UserService {
     @Override
     public void createUser(UserServiceModel userServiceModel) {
         RoleEntity userRole = roleService.findByRole(RoleEntityNameEnum.USER);
-        System.out.println();
         UserEntity user = modelMapper.map(userServiceModel,UserEntity.class);
         user.setPassword(passwordEncoder.encode(userServiceModel.getPassword()));
-        user.setPicUrl("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.subpng.com%2Fpng-9lw7jx%2F&psig=AOvVaw0c6sFiecNtCHsdCz5bBcVU&ust=1636762391519000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKCY_pvFkfQCFQAAAAAdAAAAABAD");
+        user.setPicUrl(USER_PIC_URL);
         Set<RoleEntity> roles = new HashSet<>();
         roles.add(userRole);
-
         user.setRoles(roles);
         userRepository.save(user);
     }
@@ -63,47 +56,12 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void initializeUsers() {
-        if (userRepository.count() == 0) {
-            roleService.initializeRoles();
-            RoleEntity adminRole = roleService.findByRole(RoleEntityNameEnum.ADMIN);
-            RoleEntity moderatorRole = roleService.findByRole(RoleEntityNameEnum.MODERATOR);
-            RoleEntity userRole = roleService.findByRole(RoleEntityNameEnum.USER);
-
-            UserEntity admin = new UserEntity();
-            admin.setUsername("admin");
-            admin.setPassword(passwordEncoder.encode("12345"));
-            admin.setFullName("Ivailo Netzov");
-            admin.setPicUrl("https://lh3.googleusercontent.com/proxy/Nk8iU0OPG9yZ3i_ayl5X4OBIudpt6PbCuIWd5LsR6fHXHAS5u920O07vBNCroMY4cnsBNKGMkjnlqBJSCV8");
-            admin.setEmail("admin@Gmail.com");
-            admin.setRoles(Set.of(adminRole));
-            userRepository.save(admin);
-
-            UserEntity moderator = new UserEntity();
-            moderator.setUsername("moderator");
-            moderator.setPassword(passwordEncoder.encode("12345"));
-            moderator.setFullName("Liliq Kerezova");
-            moderator.setPicUrl("https://lh3.googleusercontent.com/proxy/Nk8iU0OPG9yZ3i_ayl5X4OBIudpt6PbCuIWd5LsR6fHXHAS5u920O07vBNCroMY4cnsBNKGMkjnlqBJSCV8");
-            moderator.setEmail("moderator@Gmail.com");
-            moderator.setRoles(Set.of(moderatorRole));
-            userRepository.save(moderator);
-
-            UserEntity userAcc = new UserEntity();
-            userAcc.setUsername("userAcc");
-            userAcc.setPassword(passwordEncoder.encode("12345"));
-            userAcc.setFullName("Vesselin Netzov");
-            userAcc.setPicUrl("https://lh3.googleusercontent.com/proxy/Nk8iU0OPG9yZ3i_ayl5X4OBIudpt6PbCuIWd5LsR6fHXHAS5u920O07vBNCroMY4cnsBNKGMkjnlqBJSCV8");
-            userAcc.setEmail("user@Gmail.com");
-            userAcc.setRoles(Set.of(userRole));
-            userRepository.save(userAcc);
-
-
-        }
+        userRepository.saveAll(createUsersIfDatabaseIsEmpty());
     }
 
     @Transactional
     @Override
     public Optional<UserEntity> updateUser(long userId, String fullName) {
-        Optional<UserEntity> user = userRepository.findById(userId);
         return userRepository.findById(userId).map(target -> {
             target.setFullName(fullName);
             return target;
@@ -146,4 +104,43 @@ public class UserServiceImpl implements UserService {
         return this.modelMapper
                 .map(user, UserManageViewModel.class);
     }
+    private List<UserEntity> createUsersIfDatabaseIsEmpty(){
+        List<UserEntity> userList = new ArrayList<>();
+        if (userRepository.count() == 0) {
+            roleService.initializeRoles();
+            RoleEntity adminRole = roleService.findByRole(RoleEntityNameEnum.ADMIN);
+            RoleEntity moderatorRole = roleService.findByRole(RoleEntityNameEnum.MODERATOR);
+            RoleEntity userRole = roleService.findByRole(RoleEntityNameEnum.USER);
+
+            UserEntity admin = new UserEntity();
+            admin.setUsername("admin");
+            admin.setPassword(passwordEncoder.encode(PASSWORD));
+            admin.setFullName("Ivailo Netzov");
+            admin.setPicUrl(USER_PIC_URL);
+            admin.setEmail("admin@Gmail.com");
+            admin.setRoles(Set.of(adminRole));
+            userList.add(admin);
+
+            UserEntity moderator = new UserEntity();
+            moderator.setUsername("moderator");
+            moderator.setPassword(passwordEncoder.encode(PASSWORD));
+            moderator.setFullName("Liliq Kerezova");
+            moderator.setPicUrl(USER_PIC_URL);
+            moderator.setEmail("moderator@Gmail.com");
+            moderator.setRoles(Set.of(moderatorRole));
+            userList.add(moderator);
+
+            UserEntity userAcc = new UserEntity();
+            userAcc.setUsername("userAcc");
+            userAcc.setPassword(passwordEncoder.encode(PASSWORD));
+            userAcc.setFullName("Vesselin Netzov");
+            userAcc.setPicUrl(USER_PIC_URL);
+            userAcc.setEmail("user@Gmail.com");
+            userAcc.setRoles(Set.of(userRole));
+            userList.add(userAcc);
+        }
+            return userList;
+    }
+
+
 }
