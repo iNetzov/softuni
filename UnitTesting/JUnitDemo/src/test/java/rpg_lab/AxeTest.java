b142f9519285628d58c92cb408ae3344345d3e18
iNetzov <ivn9812@gmail.com>
2022-02-13 23:57:20 +0200

    Working on UnitTesting with jUnit still in progress

---
+++ UnitTesting/JUnitDemo/src/test/java/rpg_lab/AxeTest.java
@@ -0,0 +1,18 @@
+package rpg_lab;
+
+import org.junit.Test;
+
+public class AxeTest {
+
+
+    @Test
+    public void addTestName(){
+
+    }
+
+    @Test
+    public void testFailed(){
+        throw new AssertionError();
+    }
+
+}
