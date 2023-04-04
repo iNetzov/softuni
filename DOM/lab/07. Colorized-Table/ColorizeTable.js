e2be60900aa92c1998dff3cafe7329f97e7c473f
iNetzov <ivn9812@gmail.com>
2023-04-04 14:29:42 +0300

    DOM Lab  -Solutions from 6 to 9

--- DOM/lab/07. Colorized-Table/ColorizeTable.js
+++ DOM/lab/07. Colorized-Table/ColorizeTable.js
@@ -1,3 +1,12 @@
 function colorize() {
-    // TODO
+    let [notNeeded, ...colletion] = document.querySelectorAll('tr');
+    console.log(colletion);
+    for (let index = 0; index < colletion.length; index++) {
+        if(index % 2 == 0 ){
+           let element =  colletion[index];
+           console.log(element);
+           element.style.background= 'Teal';
+        }
+        
+    }
 }
--- DOM/lab/09. Highlight-Active/app.js
+++ DOM/lab/09. Highlight-Active/app.js
@@ -1,3 +1,11 @@
 function focused() {
-    console.log('TODO:...');
+    let [...collection] = document.querySelectorAll('input[type="text"]');
+    for (const e of collection){
+        e.addEventListener('focus', (e)=>{
+          e.target.parentNode.classList.add('focused');    
+    });
+    e.addEventListener('blur', (e)=> {
+        e.target.parentNode.classList.remove('focused');
+    }) 
+    }
 }
