b3cb486e75b6e5f7301ac62db1576f9291c7f410
iNetzov <ivn9812@gmail.com>
2024-02-04 19:19:15 +0200

    Greetings by Name solution

--- basic/Lab1/GreetingByName/GreetingByName.cpp
+++ basic/Lab1/GreetingByName/GreetingByName.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
+
+namespace personalInfo {
+	string fName = "";
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	string greeting = "Hello";
+	cin >> personalInfo::fName;
+	cout << "Hello, " << personalInfo::fName << "!" << endl;
 }
