60426ad8f46f0a023a9db892fcf8241d2decdfb7
iNetzov <ivn9812@gmail.com>
2024-02-04 19:31:09 +0200

    ConcatenateData solution

--- basic/Lab1/ConcatenateData/ConcatenateData.cpp
+++ basic/Lab1/ConcatenateData/ConcatenateData.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <string>
+using std::cout;
+using std::cin;
+using std::string;
+
+namespace personalInfo {
+	string fName = "";
+	string lName = "";
+	int age = -1;
+	string town = "";
+}
+using namespace	personalInfo;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+	cin >> fName >> lName >> age >> town;
+	cout << "You are " << fName << " " << lName << ", a " << age << "-years old person from " << town << ".";
 }
