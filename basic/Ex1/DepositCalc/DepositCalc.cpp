7b5b085490d6291595a077f509dcad21f5edc00e
iNetzov <ivn9812@gmail.com>
2024-02-15 11:54:20 +0200

    DepositCalc solution

--- basic/Ex1/DepositCalc/DepositCalc.cpp
+++ basic/Ex1/DepositCalc/DepositCalc.cpp
@@ -1,20 +1,18 @@
-// DepositCalc.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using std::cin;
+using std::cout;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int sumToDeposit = -1;
+	int months = -1;
+	double leans = -1;
+
+	cin >> sumToDeposit >> months >> leans;
+	
+	double leansPerYear = sumToDeposit * (leans/100);
+	double leansPerM = leansPerYear / 12;
+	double totalSum = sumToDeposit + (months * leansPerM);
+
+	cout << totalSum;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
