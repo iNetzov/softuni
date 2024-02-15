338248103fa2980658dc1db034a6ecceb39e45ce
iNetzov <ivn9812@gmail.com>
2024-02-15 14:29:57 +0200

    SpeedInfo solution

--- basic/Lab2/SpeedInfo/SpeedInfo.cpp
+++ basic/Lab2/SpeedInfo/SpeedInfo.cpp
@@ -1,20 +1,29 @@
-// SpeedInfo.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	double inputSpeed;
+	string output;
+	cin >> inputSpeed;
+	if (inputSpeed <= 10)
+	{
+		output = "slow";
+	}
+	else if (inputSpeed > 10 && inputSpeed <= 50)
+	{
+		output = "average";
+	}
+	else if (inputSpeed > 50 && inputSpeed <= 150)
+	{
+		output = "fast";
+	}
+	else if (inputSpeed > 150 && inputSpeed <= 1000)
+	{
+		output = "ultra fast";
+	}
+	else 
+	{
+		output = "extremely fast";
+	}
+	cout << output;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
