120243320639e9f8cad0ac07010d9a6b86b40957
iNetzov <ivn9812@gmail.com>
2024-02-15 14:19:31 +0200

    NumbersFrom100To200 solution

--- basic/Lab2/NumberFrom100To200/NumberFrom100To200.cpp
+++ basic/Lab2/NumberFrom100To200/NumberFrom100To200.cpp
@@ -1,20 +1,22 @@
-// NumberFrom100To200.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int number;
+	cin >> number;
+	string output;
+	if (number < 100) 
+	{
+		output = "Less than 100";
+	}
+	else if (number>= 100 && number <= 200) 
+	{
+		output = "Between 100 and 200";
+	}
+	else if (number > 200) 
+	{
+		output = "Greater than 200";
+	}
+	cout << output;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
