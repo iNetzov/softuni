dba037a028eff3c2152b0f5a71d44eac7469b026
iNetzov <ivn9812@gmail.com>
2024-02-15 17:08:06 +0200

    ToyStore solution

--- basic/Ex2/ToyShop/ToyShop.cpp
+++ basic/Ex2/ToyShop/ToyShop.cpp
@@ -1,20 +1,45 @@
-// ToyShop.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <iomanip>
+
+namespace constants {
+	const double PRICE_PUZZEL = 2.60;
+	const double PRICE_DOLL = 3.00;
+	const double PRICE_BEAR = 4.10;
+	const double PRICE_MINION = 8.20;
+	const double PRICE_TRUCK = 2.00;
+	const double TAXES = 10;
+
+}
+
+using namespace std;
+using namespace constants;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	double priceForVacation;
+	int puzzelCount;
+	int dollCount;
+	int bearCount;
+	int minionCount;
+	int truckCount;
+	double discount = 0;
+	
+	cin >> priceForVacation >> puzzelCount >> dollCount >> bearCount >> minionCount >> truckCount;
+	int totalCount = puzzelCount + dollCount + bearCount + minionCount + truckCount;
+	
+	if (totalCount >= 50)
+	{
+		discount = 25;
+	}
+	double totalProfit = puzzelCount * PRICE_PUZZEL + dollCount * PRICE_DOLL + bearCount * PRICE_BEAR + minionCount * PRICE_MINION + truckCount * PRICE_TRUCK;
+	totalProfit = totalProfit - totalProfit * (discount / 100);
+	totalProfit = totalProfit - totalProfit * (TAXES / 100);
+
+	cout.setf(ios::fixed);
+	if (totalProfit - priceForVacation >= 0) {
+		cout << "Yes! " <<setprecision(2) << totalProfit - priceForVacation << " lv left.";
+	}
+	else {
+		cout << "Not enough money! " << setprecision(2) << priceForVacation - totalProfit << " lv needed." << endl;
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
