d66d9ba0f5fdd231dbb673b34741d24caf977b74
iNetzov <ivn9812@gmail.com>
2024-02-15 10:51:36 +0200

    ConsoleConvertorMoney solution

--- basic/Ex1/ConsoleConvertorMoney/ConsoleConvertorMoney.cpp
+++ basic/Ex1/ConsoleConvertorMoney/ConsoleConvertorMoney.cpp
@@ -1,20 +1,22 @@
-// ConsoleConvertorMoney.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <iomanip>
+using  std::cin;
+using  std::cout;
+using std::setprecision;
+using std::fixed;
+using std::ios;
+namespace constants {
+	const double COURCE = 1.79549;
+}
+using namespace constants;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	double entryPrice = -1;
+	cin >> entryPrice;
+	double convertedPrice = entryPrice * COURCE;
+
+	cout.setf(ios::fixed);
+	
+	cout << setprecision(2) << convertedPrice;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
