b5847bf5a8cbcf43223afd476474dc8f8c8d5abe
iNetzov <ivn9812@gmail.com>
2024-02-15 11:14:26 +0200

    ConsoleConvertorRadians solution

--- basic/Ex1/ConsoleConvertorRadians/ConsoleConvertorRadians.cpp
+++ basic/Ex1/ConsoleConvertorRadians/ConsoleConvertorRadians.cpp
@@ -1,20 +1,19 @@
-// ConsoleConvertorRadians.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+#define _USE_MATH_DEFINES
 
 #include <iostream>
+#include <math.h>
+#include <cmath>
+using namespace std;
+namespace constrants {
+	const int FORMULA_VALUES = 180;
+}
+using namespace constrants;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	double radians = -1;
+
+	cin >> radians;
+	double degrees = round(radians * FORMULA_VALUES / M_PI);
+	cout << degrees;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
