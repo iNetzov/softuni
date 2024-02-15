d217aa5c2cc15b4315993d40ff36b0ac16b2435c
iNetzov <ivn9812@gmail.com>
2024-02-15 16:43:27 +0200

    TimePlus15 Solution

--- basic/Ex2/TimePlus15Minutes/TimePlus15Minutes.cpp
+++ basic/Ex2/TimePlus15Minutes/TimePlus15Minutes.cpp
@@ -1,20 +1,27 @@
-// TimePlus15Minutes.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+	int hours;
+	int minutes;
+	const int BONUS_MINUTES = 15;
+	cin >> hours >> minutes;
+
+	if (minutes + BONUS_MINUTES > 59) {
+		hours++;
+		minutes = minutes + BONUS_MINUTES;
+		minutes = minutes - 60;
+	}
+	else {
+		minutes = minutes + BONUS_MINUTES;
+	}
+	if (hours > 23) {
+		hours = hours - 24;
+	}
+	if (minutes < 10) {
+		cout << hours << ":0" << minutes;
+	}
+	else {
+		cout << hours << ":" << minutes;
+	}
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
