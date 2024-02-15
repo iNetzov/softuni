9f731c1371581e4c9d4ccb7847d6d7f0003896d7
iNetzov <ivn9812@gmail.com>
2024-02-15 13:13:20 +0200

    FoodDelivery solution

--- basic/Ex1/FoodDelivery/FoodDelivery.cpp
+++ basic/Ex1/FoodDelivery/FoodDelivery.cpp
@@ -1,20 +1,26 @@
-// FoodDelivery.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-
+namespace prices {
+    const double CHICKEN_PRICE = 10.35;
+    const double DESERT_PERSENT = 20;
+    const double FISH_PRICE = 12.40;
+    const double VEGIE_PRICE = 8.15;
+    const double DEVILERY = 2.50;
+}
+using namespace prices;
+using std::cin;
+using std::cout;
 int main()
 {
-    std::cout << "Hello World!\n";
+    int chickentCount;
+    int fishCount;
+    int vegieCount;
+    cin >> chickentCount >> fishCount >> vegieCount;
+
+    double chickentTotalPrice = chickentCount * CHICKEN_PRICE;
+    double fishTotalPrice = fishCount * FISH_PRICE;
+    double vegieTotalPrice = vegieCount * VEGIE_PRICE;
+    double totalPrice = chickentTotalPrice + fishTotalPrice + vegieTotalPrice;
+    double desertTotalPrice = totalPrice * (DESERT_PERSENT/100);
+    totalPrice = totalPrice + desertTotalPrice + DEVILERY;
+    cout << totalPrice;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
