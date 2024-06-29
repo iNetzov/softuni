12fb81486cd5b0a457be5393d9c70c9dfa206f37
iNetzov <ivn9812@gmail.com>
2024-06-29 15:19:19 +0300

    test process

--- tests/ui/grades.test.js
+++ tests/ui/grades.test.js
@@ -3,6 +3,6 @@ const { test, expect } = require('@playwright/test');
 test('Check grades page', async ({ page }) => {
     await page.goto('http://localhost:8090/My-Grades');
     const list = await page.$('ul');
-    expect(list).toBeTruthy();
+    expect(list).toBeTruthy(); 
   });
   
