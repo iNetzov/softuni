167715b2c7e2dfababb1aa88ab4145c4a949a6cf
ivailonetzov <ivn9812@gmail.com>
2025-03-22 12:24:20 +0200

    adding the keys to repo

--- DeepDiveInComponents3/react-keys/src/App.jsx
+++ DeepDiveInComponents3/react-keys/src/App.jsx
@@ -1,33 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movies, setMovie] =  useState([
+{id:1,title:'The matrix'},
+{id:2,title:'Man of Steal'},
+{id:3,title:'Spider Man'},
+{id:4,title:'Lord of the rings'},
+  ])
+  const removeFirstHandler = () => {
+    setMovie(movies.shift()); // DONT DO THIS
+    setMovie([...movies])     // Aways use new refferance, becouse it is watching for referance. When workign with objects and reference types.
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Movies List</h2>
+      <ul>{movies.map((m) => <li key={m.id}>{m.title}</li>)}</ul>
+      <button onClick={removeFirstHandler}>remove first</button>
+
     </>
   )
 }
