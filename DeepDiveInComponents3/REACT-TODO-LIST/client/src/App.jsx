9bded027857b4ecff1849c5149edef9eadcc688e
ivailonetzov <ivn9812@gmail.com>
2025-03-22 15:05:49 +0200

    commit the changes on the demo

--- DeepDiveInComponents3/REACT-TODO-LIST/client/src/App.jsx
+++ DeepDiveInComponents3/REACT-TODO-LIST/client/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import TodoList from './components/TodoList'
 
 function App() {
 
   return (
     <>
-      
+      <h1>hello from TODO LIST: </h1>
+      <TodoList/>
     </>
   )
 }
